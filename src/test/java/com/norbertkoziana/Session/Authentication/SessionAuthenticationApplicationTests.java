78163ec2d6ae34dfb34bb5210888d7cde20ecdac
Norbert Koziana <koziana11norbert@gmail.com>
2024-07-21 13:19:10 +0200

    Initial commit

---
+++ src/test/java/com/norbertkoziana/Session/Authentication/SessionAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norbertkoziana.Session.Authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
