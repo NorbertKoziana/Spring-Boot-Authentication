bf21bdd04a358119215e28f72391879506ccb332
Norbert Koziana <koziana11norbert@gmail.com>
2024-07-23 14:56:38 +0200

    Added storing sessions in redis

---
+++ src/main/java/com/norbertkoziana/Session/Authentication/sessions/SessionConfig.java
@@ -0,0 +1,32 @@
+package com.norbertkoziana.Session.Authentication.sessions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+@Configuration(proxyBeanMethods = false)
+@EnableRedisHttpSession
+public class SessionConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Value("${spring.data.redis.port}")
+    private Integer port;
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        redisStandaloneConfiguration.setPort(port);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+}
